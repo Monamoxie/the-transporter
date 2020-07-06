f7e5f36525006696d9cf0e298d9a030f515beb11
Mona Moxie <moxie4lyf@gmail.com>
2020-07-06 21:01:24 +0100

    wip: homewirelings

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import { VueSpinners } from '@saeris/vue-spinners'
 
 Vue.use(VueSpinners)
+require('./assets/css/home.css')
+// import './assets/js/three.min.js'
+// import './assets/js/postprocessing.js'
+// import './assets/js/InfiniteLights.js'
+// import './assets/js/Distortions.js' 
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,109 @@
 <template>
-    <div class="home">
-        <div class="frame">
-          <div class="frame__title-wrap">
-            <h1 class="frame__title">Infinite Lights</h1>
-            <div class="frame__links">
-              <a href="https://tympanus.net/Development/webGLImageTransitions/">Previous demo</a>
-              <a href="https://tympanus.net/codrops/?p=44516">Article</a>
-              <a href="https://github.com/Anemolo/InfiniteLights">GitHub</a>
-            </div>
-          </div>
-          <div class="frame__info">Hint: Press to speed up</div>
-          <div class="frame__demos">
-            <a href="index.html" class="frame__demo">interstate 1</a>
-            <a href="index2.html" class="frame__demo">interstate 2</a>
-            <a href="index3.html" class="frame__demo frame__demo--current">interstate 3</a>
-            <a href="index4.html" class="frame__demo">interstate 4</a>
-            <a href="index5.html" class="frame__demo">interstate 5</a>
-            <a href="index6.html" class="frame__demo">interstate 6</a>
-            <a href="index7.html" class="frame__demo">interstate 7</a>
-          </div>
-        </div>
-        <div class="content">
-          <div id="app"></div>
-          <div class="content__title-wrap">
-            <span class="content__pretitle">Indy Crossroads</span>
-            <h2 class="content__title">Voltage Race</h2>
-            <a class="content__link" href="#">Join us</a>
-          </div>
-        </div>
-    </div>
-</template>
-<style scoped>
-  @import '@assets/css/home.css';
-</style>
+     <main>
+			<div class="content">
+				<div id="runway" style="border: 2px solid red"></div>
+
+				<div class="content__title-wrap">
+					<span class="content__pretitle">Indy Crossroads</span>
+					<h2 class="content__title">Voltage Race</h2>
+					<a class="content__link" href="#">Join us</a>
+				</div>
+			</div>
+		</main>
+</template> 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-// import myKey from '../assets/js/three'
-// import myKey2 from '../assets/js/postprocessing'
-// import myKey3 from '../assets/js/InfiniteLights'
-// import myKey4 from '../assets/js/Distortions'
+import HelloWorld from '@/components/HelloWorld.vue' 
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+        // let script = document.createElement('script');
+        // script.src = '../assets/js/three.js';
+        // document.head.appendChild(script);
+
+        // script = document.createElement('script');
+        // script.src = '../assets/js/postprocessing.js';
+        // document.head.appendChild(script);
+
+        // script = document.createElement('script');
+        // script.src = '../assets/js/InfiniteLights.js';
+        // document.head.appendChild(script);
+
+        // script = document.createElement('script');
+        // script.src = '../assets/js/Distortions.js';
+        // document.head.appendChild(script);
+
+        (function() {
+
+  const container = document.getElementById('runway');
+
+    const options = {
+    onSpeedUp: (ev) => {					
+    },
+    onSlowDown: (ev) => {
+    },
+    // mountainDistortion || LongRaceDistortion || xyDistortion || turbulentDistortion || turbulentDistortionStill || deepDistortionStill || deepDistortion
+    distortion: xyDistortion, 
+    
+    length: 400,
+    roadWidth: 9,
+    islandWidth: 2,
+    lanesPerRoad: 3,
+
+    fov: 90,
+    fovSpeedUp: 150,
+    speedUp: 9,
+    carLightsFade: 0.4,
+
+    totalSideLightSticks: 50,
+    lightPairsPerRoadWay: 30,
+
+    // Percentage of the lane's width
+    shoulderLinesWidthPercentage: 0.05,
+    brokenLinesWidthPercentage: 0.1,
+    brokenLinesLengthPercentage: 0.5,
+
+    /*** These ones have to be arrays of [min,max].  ***/
+    lightStickWidth: [0.02, 0.05],
+    lightStickHeight: [0.3, 0.7],
+
+    movingAwaySpeed: [20, 50],
+    movingCloserSpeed: [-150, -230],
+
+    /****  Anything below can be either a number or an array of [min,max] ****/
+
+    // Length of the lights. Best to be less than total length
+    carLightsLength: [400 * 0.05, 400 * 0.2],
+    // Radius of the tubes
+    carLightsRadius: [0.03, 0.08],
+    // Width is percentage of a lane. Numbers from 0 to 1
+    carWidthPercentage: [0.1, 0.5],
+    // How drunk the driver is.
+    // carWidthPercentage's max + carShiftX's max -> Cannot go over 1. 
+    // Or cars start going into other lanes 
+    carShiftX: [-0.5, 0.5],
+    // Self Explanatory
+    carFloorSeparation: [0, 0.1],
+
+    colors: {
+      roadColor: 0x080808,
+      islandColor: 0x0a0a0a,
+      background: 0x000000,
+      shoulderLines: 0x131318,
+      brokenLines: 0x131318,
+      /***  Only these colors can be an array ***/
+      leftCars: [0x7D0D1B, 0xA90519, 0xff102a],
+      rightCars: [0xF1EECE, 0xE6E2B1, 0xDFD98A],
+      sticks: 0xF1EECE,
+    }
+  };
+
+  const myApp = new App(container, options);
+  myApp.loadAssets().then(myApp.init)
+})()
+    }
 }
 </script>
